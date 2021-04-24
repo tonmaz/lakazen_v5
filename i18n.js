ded7b823eee993bfeb3aa74c31ee38d72bba97f3
Tonmaz <54241333+tonmaz@users.noreply.github.com>
2021-04-24 11:27:04 +0200

    reserve

---
+++ i18n.js
@@ -0,0 +1,15 @@
+module.exports = {
+  locales: ["en", "fr"],
+  defaultLocale: "en",
+  pages: {
+    "*": ["common"],
+    "/404": ["error"],
+    "/": ["home"],
+  },
+  interpolation: {
+    prefix: "${",
+    suffix: "}",
+  },
+  loadLocaleFrom: (locale, namespace) =>
+    import(`./src/translations/${namespace}_${locale}`).then((m) => m.default),
+};
